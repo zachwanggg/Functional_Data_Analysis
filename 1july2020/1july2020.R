960619d63962c80e465d1bc9032a3e8b7cf17d5b
Zach Wang <zachwangggg@gmail.com>
2020-07-16 11:07:54 -0500

    fix cycle count issue

--- 1july2020/1july2020.R
+++ 1july2020/1july2020.R
@@ -12,45 +12,6 @@ z <- read.zoo(data_15may2020)
 data_15may2020 <- select(data_15may2020, -c(X))
 data_mat <- as.matrix(data_15may2020)
 
-#####2. same function used from last time
-fPCA_subset <- function(time_subset, data_mat, node_subset, k, nharm){
-  basis <- create.fourier.basis(c(time_subset[1],time_subset[length(time_subset)]), k)
-  smoothfd <- smooth.basis(time_subset, data_mat[time_subset,node_subset], basis)$fd
-  plot(smoothfd)
-  title(main="smoothed curves")
-  pcalist = pca.fd(smoothfd, nharm, harmfdPar=fdPar(smoothfd))
-  rotpcalist = varmx.pca.fd(pcalist)
-  par(mfrow=c(nharm,1))
-  plot.pca.fd(rotpcalist)
-  return(rotpcalist)
-}
-
-rotpcalist = fPCA_subset(time_subset=c(1:600), data_mat, node_subset = c(1,6,7,9,15)
-                         , k=281, nharm=2)
-coefdf=rotpcalist$harmonics$coefs
-
-######
-######
-######
-###### evaluate fourier basis functions
-
-tt=c(1:600)
-basis = create.fourier.basis(c(1,600),7)
-phimat = eval.basis(c(1:600),basisobj=basis)
-phi.frame = data.frame(cbind(phimat,tt))
-melt.phi = melt(data=phi.frame,id.vars="tt")
-par(mfrow=c(2,1))
-plot(melt.phi$tt, melt.phi$value)
-
-########################
-########################
-result_obj <- f_fourier_smooth(time_subset=c(1:600),data_mat
-                               ,node_subset=c(1), k=50)
-
-smoothed_curve = eval.fd(c(1:600),result_obj$fd)
-plot(smoothed_curve)
-
-
 
 ### function
 plot.periodicCycle = function(data, register, standardized){
@@ -67,27 +28,17 @@ plot.periodicCycle = function(data, register, standardized){
   i=1
   result=data.frame(cycle=integer(), time=integer(), y_value=integer())
   while (i+2<=length(z_idx)){
-    if (standardized==0){
       if(register==0){
-        tmp=data.frame(cycle=i, time=seq(z_idx[i],z_idx[i+2]), y_value=smoothed_curve[z_idx[i]:z_idx[i+2]])
+        tmp=data.frame(cycle=i, time=scale(seq(z_idx[i],z_idx[i+2]), scale=ifelse(standardized==0,FALSE,TRUE)), y_value=smoothed_curve[z_idx[i]:z_idx[i+2]])
       }
       else{
-        tmp=data.frame(cycle=i, time=seq(1,length(seq(z_idx[i],z_idx[i+2]))), y_value=smoothed_curve[z_idx[i]:z_idx[i+2]])  
+        tmp=data.frame(cycle=i, time=scale(seq(1,length(seq(z_idx[i],z_idx[i+2]))), scale=ifelse(standardized==0,FALSE,TRUE)), y_value=smoothed_curve[z_idx[i]:z_idx[i+2]])  
       }
       result=rbind(result,tmp)
-    }
-    else{
-      if(register==0){
-        tmp=data.frame(cycle=i, time=scale(seq(z_idx[i],z_idx[i+2])), y_value=smoothed_curve[z_idx[i]:z_idx[i+2]])
-      }
-      else{
-        tmp=data.frame(cycle=i, time=scale(seq(1,length(seq(z_idx[i],z_idx[i+2])))), y_value=smoothed_curve[z_idx[i]:z_idx[i+2]]) 
-      }
-      result=rbind(result,tmp)
-    }
-    i=i+2
+      i=i+2
   }
   ggplot(result, aes(time, y_value,group=cycle, colour=cycle)) + geom_line() + theme(legend.position="top")
+  return(result)
 }
 
 
@@ -100,12 +51,7 @@ f_fourier_smooth <- function(time_subset, data_mat, node_subset, k){
   return(fd_obj)
 }
 
-
-#result_obj <- f_fourier_smooth(time_subset=c(1:600), data_mat, node_subset = c(1)
-#                         ,k=selection_result[which.min(selection_result[,2]),1],   
-#                         nharm=2)
-
 result_obj <- f_fourier_smooth(time_subset=c(1:600), data_mat, node_subset=c(1), k=50)
 smoothed_curve = eval.fd(c(1:600),result_obj$fd)
 plot.periodicCycle(data=smoothed_curve, register=1, standardized=1)
-plot.periodicCycle(data=smoothed_curve, register=1, standardized=1)
+result = plot.periodicCycle(data=smoothed_curve, register=0, standardized=0)
