5d77485f08ccb99b8e685d65868f5926bb0d0a49
Zach Wang <zachwangggg@gmail.com>
2020-08-06 14:20:23 -0500

    aug6

--- 26july2020/26july2020_v1.R
+++ 26july2020/26july2020_v1.R
@@ -1,6 +1,4 @@
 # find y value based on PC coef and sin/cos functions
-
-
 PC1_df = data.frame(matrix(nrow=80))
 PC2_df = data.frame(matrix(nrow=80))
 for(node in 1:32){
@@ -36,6 +34,7 @@ for(node in 1:32){
     df_new <- as.matrix(df_new)
     rotpcalist = fPCA_subset(df_new, k=11, nharm=2, plt=0)
     PCA_df <- data.frame(matrix(nrow=rotpcalist$harmonics$basis$rangeval[2]))
+    # Calculation for Harmonics/eigenvectors
     for(x in (rotpcalist$harmonics$basis$rangeval[1]:rotpcalist$harmonics$basis$rangeval[2])){
       PC1_coef = rotpcalist$harmonics$coefs[,1]
       PC2_coef = rotpcalist$harmonics$coefs[,2]
@@ -44,15 +43,36 @@ for(node in 1:32){
       odd_idx = (1:length(PC1_coef))[flag==0][-1]
       PCA_df[x, 1] = PC1_coef[1] + sin(x)*sum(PC1_coef[even_idx]) + cos(x)*sum(PC1_coef[odd_idx]) # fixme
       PCA_df[x, 2] = PC2_coef[1] + sin(x)*sum(PC2_coef[even_idx]) + cos(x)*sum(PC2_coef[odd_idx]) # fixme
+    
+        ## calculation trial 2
+      #PCA_df[x, 1] = PC1_coef[1]
+      #PCA_df[x, 2] = PC2_coef[1]
+      #t=1
+      #for(i in 1:length((even_idx))){
+      #  PCA_df[x, 1]=PCA_df[x, 1]+sin(t*x*PC1_coef[even_idx[i]])
+      #  PCA_df[x, 2]=PCA_df[x, 2]+sin(t*x*PC2_coef[even_idx[i]])
+      #  t=t+1
+      #}
+      #t=1
+      #for(j in 1:length((odd_idx))){
+      #  PCA_df[x, 1]=PCA_df[x, 1]+sin(t*x*PC1_coef[odd_idx[j]])
+      #  PCA_df[x, 2]=PCA_df[x, 2]+sin(t*x*PC2_coef[odd_idx[j]])
+      #  t=t+1
+      #}
+      
     }
 PC1_df[,ncol(PC1_df)+1]=PCA_df[,1]    
 PC2_df[,ncol(PC2_df)+1]=PCA_df[,2]
 }
 
-PC1_df=PC1_df[,2:(ncol(PC1_df))]
-PC2_df=PC2_df[,2:(ncol(PC2_df))]
+PC1_df = data.frame(PC1_df[,2:(ncol(PC1_df))])
+names(PC1_df)=colnames(data_mat)
 
+PC2_df = data.frame(PC2_df[,2:(ncol(PC2_df))])
+names(PC2_df)=colnames(data_mat)
 
-
+z_1 = read.zoo(PC1_df, index='index')
+z_2 = read.zoo(PC2_df, index='index')
+autoplot(z_1, facet = NULL)
 
 #fPCA_subset(mean_df, k=4, nharm=2, plt=0)
